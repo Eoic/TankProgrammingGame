77018cd5e7e93e4205e3f6d12dc4cac70a51bb76
unknown <razuleviciusmantvydas@gmail.com>
2018-03-20 16:21:58 +0200

    Posible create new robot

--- routes/robot_manager.js
+++ routes/robot_manager.js
@@ -2,15 +2,18 @@ var database = require('./db_connect');
 
 // Add robot record to database.
 exports.add = function (req, res) {
-    var name = req.body.robot;
-    var date = new Date();
-    database.connection.query("INSERT INTO Robots(RobotID,Name,Created,Code) VALUES ('3','dsadf','0001-01-01 00-11-03',NULL)", function (err) {
+    var insertionObject = {
+        Name: req.body.robot,
+        Created: new Date(),
+        Code: ''
+    }
+    database.connection.query("INSERT INTO Robots SET ?", insertionObject, function (err) {
         if (err) {
-            console.log("Failed insert to DB" + err);
-            req.redirect("/robots");
+            console.log("Failed insert to DB =>" + err);
         }
     });
 }
+
 
 // Update robot script.
 exports.injectLogic = function (req, res) {
