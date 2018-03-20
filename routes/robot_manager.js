3236b325564163c116677df0c8cd4272383812a6
unknown <razuleviciusmantvydas@gmail.com>
2018-03-20 19:39:17 +0200

    delete and create robots for specified users

--- routes/robot_manager.js
+++ routes/robot_manager.js
@@ -5,13 +5,15 @@ exports.add = function (req, res) {
     var insertionObject = {
         Name: req.body.robot,
         Created: new Date(),
-        Code: ''
+        Code: '',
+        Owner: req.session.username
     }
     database.connection.query("INSERT INTO Robots SET ?", insertionObject, function (err) {
         if (err) {
             console.log("Failed insert to DB =>" + err);
         }
     });
+    res.redirect('/robots');
 }
 
 
@@ -22,5 +24,17 @@ exports.injectLogic = function (req, res) {
 
 // Delete robot record.
 exports.delete = function (req, res) {
-
+    var name = req.body.removeRobot;
+    var user = req.session.username;
+    database.connection.query("SELECT * FROM Robots WHERE Owner = '" + user + "' and Name = '" + name + "'", function (err, result) {
+        if (err) {
+            console.log("can't find robot for this user =>" + err);
+        } else {
+            database.connection.query("DELETE FROM Robots WHERE Owner = '" + user + "' and Name = '" + name + "'", function (err) {
+                if (err) {
+                    console.log("we can found this robot for this user =>" + err);
+                }
+            })
+        }
+    });
 }
