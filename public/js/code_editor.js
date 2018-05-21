3e840e6f8a71ab18a6db848ec46f492605191160
Eoic <karolis.strazdas@ktu.edu>
2018-05-21 21:41:30 +0300

    Small fixes. New map background.

--- public/js/code_editor.js
+++
@@ -1,7 +0,0 @@
-var editor = CodeMirror(document.getElementById('editor'), {
-    lineNumbers: true,
-    value: "console.log('Editor');",
-    mode: "javascript",
-    lineWrapping: true,
-    theme: 'mbo'
-});
